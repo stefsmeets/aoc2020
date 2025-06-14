378268d7ed2281502ae0578c5d0611d62edff6e6
Stef Smeets <s.smeets@esciencecenter.nl>
2025-06-14 10:43:11 +0200

    First commit

---
+++ day01/Program.cs
@@ -0,0 +1,44 @@
+// See https://aka.ms/new-console-template for more information
+string path = "data.txt";
+
+var lines = File.ReadLines(path);
+
+List<int> numbers = new List<int>();
+
+foreach (var line in lines)
+    {
+        int x = int.Parse(line);
+        numbers.Add(x);
+    }
+
+int target = 2020;
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    int x = numbers[i];
+    for (int j = i; j < numbers.Count; j++)
+    {
+        int y = numbers[j];
+        if (x + y == target)
+        {
+            Console.WriteLine($"Part 1: {x} * {y} = {x*y}");
+        }
+    }
+}
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    int x = numbers[i];
+    for (int j = i; j < numbers.Count; j++)
+    {
+        int y = numbers[j];
+        for (int k = j; k < numbers.Count; k++)
+        {
+            int z = numbers[k];
+            if (x + y + z == target)
+            {
+                Console.WriteLine($"Part 2: {x} * {y} * {z} = {x*y*z}");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# AOC 2020 in C#
+
+Requirements: dotnet-9.0
+
+To run:
+
+```bash
+dotnet new console
+dotnet run
+```
